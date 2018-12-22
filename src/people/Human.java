aa4ad251a38e7a4d11600974774324f2cf1d1828
draconetics <marioflorescondori@gmail.com>
2018-12-21 23:37:08 -0400

    Added inherintance of people
    Created abstract clases of people that is going to enter to the store

---
+++ src/people/Human.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package people;
+
+/**
+ * @author dracon
+ *
+ */
+public class Human {
+
+	public static void walk() {
+		System.out.println("Human Walks.");
+	}
+	
+	public void talk() {
+		System.out.println("Human talk.");
+	}
+}
